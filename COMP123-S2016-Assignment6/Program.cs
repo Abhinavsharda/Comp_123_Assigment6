fdf4e04aa360117fb6c7870abafe56998018173c
Iqbal <Iqbal@DESKTOP-U95HP1P>
2016-08-08 19:27:44 -0400

    Add project files.

---
+++ COMP123-S2016-Assignment6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2016_Assignment6
+{
+   public static class Program
+    {
+        public static BmiCalculator FirstForm;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            FirstForm = new BmiCalculator();
+
+            Application.Run(FirstForm);
+        }
+    }
+}
